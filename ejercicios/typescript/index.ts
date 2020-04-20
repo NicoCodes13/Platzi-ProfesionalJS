cce4d3cc9885ccacb080fc7e6bec6b9fccde8594
Nico Castañeda <nickcar13@gmail.com>
2020-04-20 17:00:03 -0500

    Typescript variables basicas

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,39 @@
-console.log("Hello, Typescript")
+//bollean 
 
-function add(a: number, b: number) {
-    return a + b;
+let muted: boolean = true;
+muted = false;
+
+// Numeros
+let numerador:number = 42;
+let denominador:number = 6;
+let resultado = numerador / denominador;
+
+//string 
+let nombre: string = 'Richard';
+let saludo = `Me llamo richard ${nombre}`;
+
+//Arreglos 
+let people: string[] = [];
+people = ["Isabel","Nicole","Raul"];
+
+let peopleAndNumbers: Array< string | number> = [];
+peopleAndNumbers.push("Ricardo");
+peopleAndNumbers.push(2);
+
+//enum
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul",
 }
 
-const sum = add(2, 3);
+let colorFavorito:Color = Color.Verde;
+console.log(`Color favorito es ${colorFavorito}`);
+
+//Any
+let comodin:any = "Joker"
+comodin = {type: "Wildcard"}
+
+//Object
+
+let someObject: object = {type: "Wildcard"}
